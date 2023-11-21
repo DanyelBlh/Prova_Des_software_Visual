60a9979e3039a649ada1a7b61337bc7225919085
Danyel Bielah <107011798+DanyelBlh@users.noreply.github.com>
2023-11-20 21:44:39 -0300

    provaDesenvolvimentoSoftware

---
+++ API/Controllers/TarefaController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers;
+
+[Route("api/tarefa")]
+[ApiController]
+public class TarefaController : ControllerBase
+{
+    private readonly AppDataContext _context;
+
+    public TarefaController(AppDataContext context) =>
+        _context = context;
+
+    // GET: api/tarefa/listar
+    [HttpGet]
+    [Route("listar")]
+    public IActionResult Listar()
+    {
+        try
+        {
+            List<Tarefa> tarefas = _context.Tarefas.Include(x => x.Categoria).ToList();
+            return Ok(tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // POST: api/tarefa/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            Categoria? categoria = _context.Categorias.Find(tarefa.CategoriaId);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            tarefa.Categoria = categoria;
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+            return Created("", tarefa);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [Route("alterar/{id}")]
+    public IActionResult Alterar([FromRoute] int id,
+        [FromBody] Tarefa status)
+    {
+        try
+        {
+            Tarefa? TarefaCadastrada =
+            _context.Tarefas.FirstOrDefault(x => x.TarefaId == id);
+            
+            if(TarefaCadastrada == null)
+            {
+                 return NotFound();
+            }
+
+             TarefaCadastrada.Status = "Em andamento"; 
+            _context.SaveChanges();
+
+            return Ok(TarefaCadastrada);
+            
+            }
+        catch (Exception e)
+            {
+
+            return BadRequest(e.Message);
+            }
+        }
+    }
+
---
+++ API/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public DateTime CriadoEm { get; set; } = DateTime.Now;
+
+  
+
+}
+
